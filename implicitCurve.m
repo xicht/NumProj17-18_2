09df1ab5ee75640790a588312915e62866cc6ba5
Lukas M <moser.lux@gmail.com>
2018-01-06 20:41:38 +0100

    implicit Curve um Correctorschritt erweitert

--- implicitCurve.m
+++ implicitCurve.m
@@ -5,11 +5,13 @@ function [ output_args ] = implicitCurve( F, x0, y0, steps, stepWidth )
 assert(isZero(F(x0, y0)));
 
 x = x0 + stepWidth * (0:steps);
-y = zeros(steps+1);
+y = zeros(1,steps+1);
 y(1) = y0;
 
 for i = 2:steps+1
-    y(i) = ImplicitDiff(F, x(i-1), y(i-1), 0.00001);
+    y(i) = ImplicitDiff(F, x(i-1), y(i-1), 0.00001);    %predictor
+    f = @(z)F(x(i), z);
+    y(i) = Newton(f, y(i));                             %corrector
 end
 
 end
