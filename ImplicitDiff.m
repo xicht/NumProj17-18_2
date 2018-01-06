77a0fa5c881a3c5e57df05d6a85860b86984313c
Lukas M <moser.lux@gmail.com>
2018-01-06 20:40:56 +0100

    myDiff aus ImplicitDiff herausgeholt

--- ImplicitDiff.m
+++ ImplicitDiff.m
@@ -17,27 +17,3 @@ d = -Fx/Fy;
 
 end
 
-function [ phi2 ] = myDiff( F, x, w )
-global epsZero;
-phi2 = (F(x+w)-F(x))/w;
-while(true)
-    w = w/2;
-    phi1 = phi2;
-    phi2 = (F(x+w)-F(x))/w;
-    if(diffLoopCheck(phi1, phi2, epsZero))
-        break;
-    end
-end
-
-end
-
-function [ retval ] = diffLoopCheck( phi1, phi2, tau )
-    retval = true;
-    phidiff = abs(phi1 - phi2);
-    
-    if(phi1 <= tau)
-        retval = (phidiff <= tau);
-    else
-        retval = (phidiff <= (tau * abs(phi1)));
-    end
-end
---
+++ myDiff.m
@@ -0,0 +1,26 @@
+function [ phi2 ] = myDiff( F, x, w )
+%MYDIFF Summary of this function goes here
+%   Detailed explanation goes here
+global epsZero;
+phi2 = (F(x+w)-F(x))/w;
+while(true)
+    w = w/2;
+    phi1 = phi2;
+    phi2 = (F(x+w)-F(x))/w;
+    if(diffLoopCheck(phi1, phi2, eps*10^5))
+        break;
+    end
+end
+
+end
+
+function [ retval ] = diffLoopCheck( phi1, phi2, tau )
+    retval = true;
+    phidiff = abs(phi1 - phi2);
+    
+    if(abs(phi1) <= tau)
+        retval = (phidiff <= tau);
+    else
+        retval = (phidiff <= (tau * abs(phi1)));
+    end
+end
