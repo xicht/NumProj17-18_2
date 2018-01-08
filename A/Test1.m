8a6d457e373953bf488ac6b4e3fa0c715ed69dac
Unknown <stefan.schrott@outlook.com>
2018-01-08 20:39:30 +0100

    plotter, um mit matlab-plot ergebnisse kontrollieren zu können

--- A/Test1.m
+++ A/Test1.m
@@ -2,6 +2,9 @@
 global epsZero;
 epsZero = 5*eps;
 
-G = @(x,y)sin(x^2)-y
-[x y] = implicitCurve(G, 0, 0, 10000, pi/1000);
+%G = @(x,y)sin(x^2)-y
+G = @(x,y) sin(10*(sin(x)-y))
+
+
+[x y] = implicitCurve(G, 0, 0, 20, pi/100);
 plot(x,y);
