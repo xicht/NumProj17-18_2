e61523fc6b23e209340c7ce57c00d6647d34ce4e
Lukas M <moser.lux@gmail.com>
2018-01-09 13:18:24 +0100

    Automatische Berechnung des relevanten Bereichs im Test

--- A/Test2.m
+++ A/Test2.m
@@ -3,7 +3,9 @@ global epsZero;
 epsZero = 1000*eps;
 
 G = @(x,y) x.^2+y.^2-1
- 
-[x y] = implicitCurve(G, sin(0.3),cos(0.3), 704, 10^-3);
+stepwidth = 10^-4
+x0 = sin(0.2)
+y0 = cos(0.2)
+[x y] = implicitCurve(G, x0, y0, floor((1-x0)/stepwidth -epsZero), stepwidth);
 plot(x,y);
 %plot(x,G(x,y));
