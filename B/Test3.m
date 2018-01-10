df424d752e83f94a8d76b1f5da97679b23be3a68
Stefan <stefan.schrott@outlook.com>
2018-01-10 22:29:29 +0100

    missl mehr mehrumtesten

--- B/Test3.m
+++ B/Test3.m
@@ -15,7 +15,7 @@ for i = k
 end
 hold off
 
-c=10;
+c=10*pi;
 G = @(x,y) sin(c*(sin(x)-y));
 dGx = @(x,y) c*cos(sin(x)-y)*cos(x) 
 dGy = @(x,y) -c*cos(sin(x)-y) 
@@ -30,10 +30,20 @@ end
 hold off
 
 figure(3)
-k = [ 0.3 0.31  1 2 10 ]% 0.9 0.95 1 10 100]
+k = [ pi/2 pi/2+eps pi/2+3*10^-14 ]  %pi/2+10^-12 is auch witzig
 for i = k
-    [x y] = implicitCurve(G, dGx, dGy, 0, 0, floor(20*i), pi/(2*i));
+    [x y] = implicitCurve(G, dGx, dGy, 0, 0, floor(50/i), i);
     plot(x,y);
     hold on
 end
 hold off
+
+figure(4)
+k = pi/2+ 10^-14*[0 1 2 3 4 7 8 10]  %5, 6 , 9  und 10 
+for i = k
+    [x y] = implicitCurve(G, dGx, dGy, 0, 0, floor(50/i), i);
+    plot(x,y);
+    hold on
+end
+hold off
+
