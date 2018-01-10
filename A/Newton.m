b21f0dcab2cb565446e0a2ca9465d79fba7fb01e
Stefan <stefan.schrott@outlook.com>
2018-01-10 22:40:50 +0100

    Newton bricht nach 100 Iterationen mit Warnung ab.
    Kein Test mit annähernd sinnvoller Schrittweite hat dazu geführt, dass mehr als 4 Iterationen gebraucht wurden, meist ist es genau einer (oder sogar keiner)

--- A/Newton.m
+++ A/Newton.m
@@ -10,11 +10,17 @@ assert(~isZero(f(x)));
 x1 = x;
 x2 = x - F(x)/f(x);
 
+iters=0;
 while(~isZero(x1-x2) && ~isZero((x1-x2)/x1) && ~isZero(F(x2)))
 x1 = x2;
 assert(abs(f(x2)) ~= Inf);
 assert(f(x2) ~= 0);
 x2 = x2 - F(x2)/f(x2);
+iters=iters+1;
+if iters == 100
+    warning('Newton probably wrong');
+    break;
+end
 end
 
 
--- A/Test2.m
+++ A/Test2.m
@@ -9,6 +9,7 @@ stepwidth = 10^-4
 phi = -pi/2 + 0.001
 x0 = sin(phi)
 y0 = cos(phi)
-[x y] = implicitCurve(G, dGx, dGy, x0, y0, floor((1-x0)/stepwidth -epsZero), stepwidth);
+%[x y] = implicitCurve(G, dGx, dGy, x0, y0, floor((1-x0)/stepwidth -epsZero), stepwidth);
+[x y] = implicitCurve(G, dGx, dGy, sin(0.1), cos(0.1), floor((1-sin(0.1))/stepwidth -epsZero)+1, stepwidth);
 plot(x,y);
 %plot(x,G(x,y));
