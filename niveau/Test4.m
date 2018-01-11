2382bb8fd7246d62f85806b0d765bb25429f8f4c
Stefan <stefan.schrott@outlook.com>
2018-01-11 06:24:45 +0100

    Niveaulinien mit vorgebenen Funktionswerten (unferitg)
    vermutlich ein Bug in NSt-Suche im R^2 schuld

--- niveau/Test4.m
+++ niveau/Test4.m
@@ -1,5 +1,29 @@
-F = @(x,y) x.^2+y.^2-1;
 
-[x0,y0,err]=findZero (F, -0.5, 0.5, -1, 1);
+global epsZero;
+epsZero = 100*eps;
 
-F(x0,y0)
+G = @(x,y) x.^2+y.^2
+dGx = @(x,y) 2*x
+dGy = @(x,y) 2*y
+
+
+Z=[1];
+Steps=300*[1:1];
+StepWidth=5*10^-3*ones(1,1);
+
+a=-0.6;
+b=-0.4;
+c=-3;
+d=3;
+
+[X, Y] = nivlines3(G, dGx, dGy, Z, a, b, c, d, Steps, StepWidth); 
+
+figure(1)
+plot(X{1},Y{1})
+hold on
+
+for j=2:length(Z)
+   plot(X{j},Y{j});
+end
+
+hold off
