c8bbbb3b7a460f4b24ddfaeb80ccbfe87c517ebd
Lukas M <moser.lux@gmail.com>
2018-01-22 23:29:22 +0100

    Ausarbeitung A, Halbkreis

--- A/Test1.m
+++ A/Test1.m
@@ -6,5 +6,5 @@ G = @(x,y) sin(x^2)-y
 dGx = @(x,y) 2*cos(x^2)
 dGy = @(x,y) -1
 
-[x y] = implicitCurve(G, dGx, dGy, 0, 0, 1000, 5*10^-2);
+[x y] = implicitCurve(G, dGx, dGy, 0, 0, 2000, 10^-2);
 plot(x,y);
