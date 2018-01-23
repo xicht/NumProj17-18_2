fbcf5ff395e1fd6ec862d4c583977a0fd71ee454
Lukas M <moser.lux@gmail.com>
2018-01-24 00:14:40 +0100

    Ausarbeitung B Tests, C eingefügt, halber Satz geschrieben.

--- C/Test2_3.m
+++ C/Test2_3.m
@@ -5,21 +5,21 @@ epsZero = 1000*eps;
 r = 1;
 G = @(x,y) x+y+x.^10+y.^10-2
 dGx = @(x,y) 1+10*x^9
-dGy = @(x,y) -1+10*y^9
+dGy = @(x,y) 1+10*y^9
 stepwidth = 10^-2
 phi = 0
 x0 = 0
 y0 = 1
-h = 10^-2
-[x y] = implicitCurve(G, dGx, dGy, x0, y0, 7.65, h,h);  
+h = 10^-1
+[x y] = implicitCurve(G, dGx, dGy, x0, y0, 8, h,h);  
 
 plot(x,y);
 hold on
-[x y] = implicitCurve_noCorr(G, dGx, dGy, x0, y0, 7.6, h,h); 
+[x y] = implicitCurve_noCorr(G, dGx, dGy, x0, y0, 8, h,h, [1;-1]); 
 plot(x,y);
 
-[x y] = implicitCurve_ortho(G, dGx, dGy, x0, y0, 750, h); 
-plot(x,y);
-[x y] = implicitCurve_ortho_noCorr(G, dGx, dGy, x0, y0, 750, h); 
+[x y] = implicitCurve_ortho(G, dGx, dGy, x0, y0, 80, h); 
+%plot(x,y);
+[x y] = implicitCurve_ortho_noCorr(G, dGx, dGy, x0, y0, 80, h); 
 plot(x,y);
 hold off
---
+++ C/Test5.m
@@ -0,0 +1,23 @@
+global epsZero;
+global print_error;
+epsZero = 1000*eps;
+print_error = true;
+
+plottinator = @(a,b) plot(a,b,'r');
+r = 1;
+G = @(x,y) x^3-x*y+y^2-1
+dGx = @(x,y) 3*x^2-y
+dGy = @(x,y) 2*y-x
+stepwidth = 10^-2
+phi = 0
+x0 = -1
+y0 = 1
+h = 10^-1
+hold on
+[x y] = implicitCurve(G, dGx, dGy, x0, y0, 5, h,h,[1;0.1]);  
+
+plot(x,y);
+
+[x y] = implicitCurve_ortho(G, dGx, dGy, x0, y0, 50, h,plottinator); 
+plot(x,y);
+hold off
